f4b9924aeeccdb5638417dbfc55243c2d3af6ce9
Walla-B <81977508+Walla-B@users.noreply.github.com>
2021-07-24 19:26:41 +0900

    W1_C16

--- Pandas_test/test_p_4.py
+++ Pandas_test/test_p_4.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 df = pd.DataFrame(data=[[100, 20.3], [200, 15.1], [300, 25.9]], index=['idx_a', 'idx_b', 'idx_c'], columns=['price', 'weight'])
 print(df, "\n")
@@ -24,3 +25,32 @@ for col, item in df.iteritems():
 # iterate as tuple
 for row in df.itertuples():  # index=False to exclude index
     print(row)
+
+# add new empty column
+df["distance"] = ""
+
+'''
+for idx, row in df.iterrows():
+    if row['weight'] >= 20:
+        row["distance"] = 30
+    else:
+        row["distance"] = 10
+'''
+
+# doing with a better way
+
+# resetting index for easier access
+df.reset_index(drop=True, inplace=True)
+
+for i in df.index:
+    if df.loc[i]['weight'] >= 20:
+        df.at[i, 'distance'] = 30
+    else:
+        df.at[i, 'distance'] = 0
+
+
+print("after modificatiion\n")
+
+
+df.to_csv("C:/Users/dev/Desktop/2010pandas.csv", sep=',', na_rep='NaN', encoding='EUC-KR')
+
